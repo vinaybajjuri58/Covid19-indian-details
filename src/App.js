45245dd9e51921ef755edcbc5c9a3cdcc1e84c06
vinaybajjuri58 <vinaybajjuri58@github.com>
2020-07-22 01:59:09 +0530

    Without styling

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import MainComponent from './components/MainComponent' ;
+import { ConfigureStore } from './redux/configureStore';
+import { Provider } from 'react-redux';
+import HeaderComponent from './components/HeaderComponent';
+
+const store = ConfigureStore();
+
+// store.subscribe(()=> console.log(store.getState()))
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        <HeaderComponent />
+        <MainComponent />
+      </div>
+    </Provider>
   );
 }
 
